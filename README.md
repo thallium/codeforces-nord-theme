7d7fc3e4e777766fac611fb8873746d3c995b5be
Gengchen Tuo <tuogengchen@gmail.com>
2021-12-23 22:52:45 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Codeforces Nord Theme
+
+A dark color theme for codeforces using the [Nord](https://www.nordtheme.com/) color palette.
+![](demo.png)
---
+++ codeforces.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Codeforces Nord theme
+// @version      1.0.0
+// @description  Nord color theme for codeforces
+// @author       Thallium54
+// @match        https://codeforces.com/*
+// @match        http://codeforces.com/*
+// @resource     NordCSS  nord.css
+// @resource     syntax  syntax.css
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @run-at       document-start
+// ==/UserScript==
+(function() {
+    'use strict';
+    var style = GM_getResourceText("NordCSS");
+    var syntax = GM_getResourceText("syntax");
+	GM_addStyle(style);
+	GM_addStyle(syntax);
+})()

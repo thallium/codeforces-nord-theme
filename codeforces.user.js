ab0a01de90621a77cda81283fb12eb3654435e2f
Gengchen Tuo <tuogengchen@gmail.com>
2021-12-24 18:34:39 -0700

    fix color of solved problem

--- codeforces.user.js
+++ codeforces.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codeforces Nord theme
-// @version      1.0.1
+// @version      1.0.2
 // @description  Nord color theme for codeforces
 // @author       Thallium54
 // @match        https://codeforces.com/*

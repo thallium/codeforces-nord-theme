845b1261077a739367ad467a86dec8306cd03753
Gengchen Tuo <tuogengchen@gmail.com>
2021-12-28 22:14:55 -0700

    fix copy button color

--- codeforces.user.js
+++ codeforces.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codeforces Nord theme
-// @version      1.0.3
+// @version      1.0.4
 // @description  Nord color theme for codeforces
 // @author       Thallium54
 // @match        https://codeforces.com/*

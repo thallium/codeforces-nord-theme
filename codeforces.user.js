674d3051b0bca0fc9a6172fa2a577c9dd9ada8f3
Gengchen Tuo <tuogengchen@gmail.com>
2022-01-02 21:31:10 -0700

    fix color for icon and input

--- codeforces.user.js
+++ codeforces.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codeforces Nord theme
-// @version      1.0.5
+// @version      1.0.6
 // @description  Nord color theme for codeforces
 // @author       Thallium54
 // @match        https://codeforces.com/*
